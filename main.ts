793ef4d54404965e7d388659fabeafb1c6d6dd36
John Kim <querobinesj@gmail.com>
2023-02-10 19:08:45 +0800

    Initial commit

--- main.ts
+++ main.ts
@@ -1,137 +1,86 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, Plugin } from 'obsidian';
+import { marked } from 'marked';
+import makeSlug from "./utils/makeSlug"
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
-		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			"id": "obsidian-html-converter",
+			"name": "Martin's HTML Converter",
+			editorCallback: (editor: any) => this.markdownToHTML(editor)
+		})
+	}
+	
+	markdownToHTML(editor: Editor) {
+		const prepended_url = 'https://salesflowcoach.app/vault/';
+		const yamlRegex = /^---[\r\n]+([\s\S]*)[\r\n]+---[\r\n]+/;
+		const wikilink_pattern = /\[\[(.*)\]\]/g;
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		const selectedMarkdown = editor.getSelection();
+		const cleanSelection = selectedMarkdown.replace(yamlRegex, ''); // This is if yaml/fm is also selected
+		const convertedHTML = marked(cleanSelection).replace(wikilink_pattern, (_, p1) => {
+			return `<a href="${prepended_url}${makeSlug(p1)}">${p1}</a>`; // Simulate GG's way of creating slugs
+		});
+
+		const styles = `
+			<style type="text/css">
+				@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Roboto&display=swap');
+				body {
+					font-family: 'Roboto', sans-serif;
 				}
-			}
+				h1, h2, h3 {
+					font-family: 'Playfair Display', serif;
+					font-weight: 600;
+				}
+				h1, a {
+					color: #405aac;
+				}
+				h2, h3 {
+					color: #54595F;
+				}
+				h1 {
+					font-size: 2.074rem;
+				}
+				h2 {
+					font-size: 1.728rem;
+				}
+				h3 {
+					font-size: 1.44rem;
+				}
+				a {
+				    text-decoration: none;
+				}
+			</style>
+		`
+
+		const html = `
+			<html>
+				<head>
+					${styles}
+				</head>
+				<body>
+					${convertedHTML}
+				</body>
+			</html>
+		`;
+
+		const blob = new Blob([html], {
+			//@ts-ignore
+			type: ["text/plain", "text/html"]
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		const data = [new ClipboardItem({
+			["text/plain"]: blob,
+			["text/html"]: blob
+		})];
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		navigator.clipboard.write(data);
 	}
 
 	onunload() {
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
